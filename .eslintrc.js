70ad4bf74758c70a648d51418123e87ae59dab16
Steven Z <Steven@Zhao.io>
2019-02-09 18:11:05 -0800

    Add Airbnb-style linting

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "env": {
+        "es6": true
+    }
+};
--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require('express');
-const next    = require('next');
+const next = require('next');
 
-const dev     = process.env.NODE_ENV !== 'production'
-const app     = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.prepare()
-    .then(() => {
-        const server = express()
+  .then(() => {
+    const server = express();
 
-        server.get('*', (req, res) => {
-            return handle(req, res)
-        })
+    server.get('*', (req, res) => handle(req, res));
 
-        server.listen(PORT, () => {
-            console.log(`> Ready on http://localhost:${PORT}`)
-        })
-
-    })
-    .catch((ex) => {
-        console.error(ex.stack)
-        process.exit(1)
-    })
+    server.listen(PORT, () => {
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
