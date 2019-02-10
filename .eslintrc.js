eb6aeeda3e6693a528809aa05b28dd53740994dd
Steven Z <Steven@Zhao.io>
2019-02-09 18:16:03 -0800

    Update lint configuration to support JSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
     "extends": "airbnb-base",
     "parserOptions": {
-        "ecmaVersion": 2017
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "jsx": true
+          },
+        "sourceType": "module"
     },
     "env": {
-        "es6": true
+        "es6": true,
+        "browser": true,
+        "commonjs": true
+    },
+    "plugins": ["react"],
+    "rules": {
+      "react/prop-types": 0,
+      "react/jsx-uses-vars": [2],
+      "no-console": 0
     }
 };
+
